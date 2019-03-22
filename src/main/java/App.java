206279eb5a469e0b2081c0d4b8d2f09384a2d057
vtrev <baloyi.trev@gmail.com>
2019-03-22 13:02:27 +0200

    added the greeter class,seperating the greeting from the menu

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,15 +1,16 @@
+import greet.Greeter;
+
 import java.util.Scanner;
 
 public class App {
     public static void main(String[] args){
         boolean menu = true;
-        Greeting greeting = new Greeting();
+        Greeter greeting = new Greeter();
         Scanner scanner = new Scanner(System.in);
 
         while (menu){
             System.out.println("Enter a command : ");
-            String command = scanner.nextLine();
-            menu = greeting.commandInit(command);
+
         }
     }
 }
