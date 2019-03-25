5f9a6359634f55efabbe12be9454220c9ddb0d18
vtrev <baloyi.trev@gmail.com>
2019-03-25 10:39:41 +0200

    added a welcome message, changed boolean menu to showMenu

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -2,15 +2,15 @@ import java.util.Scanner;
 
 public class App {
     public static void main(String[] args){
-        boolean menu = true;
+        boolean showMenu = true;
         Scanner scanner = new Scanner(System.in);
         Greeter greeter = new Greeter();
         Counter counter = new Counter();
         CommandProcessor cpu = new CommandProcessor(greeter,counter);
-
-        while (menu){
+        System.out.println("Hello World! Welcome to the greeting app! :)");
+        while(showMenu){
             System.out.println("Enter a command : ");
-            menu =  cpu.processInput(scanner.nextLine());
+            showMenu =  cpu.processInput(scanner.nextLine());
         }
     }
 }
