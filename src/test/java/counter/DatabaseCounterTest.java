4226c645d03fe88e5ab1e025cf29b1438c75a393
vtrev <baloyi.trev@gmail.com>
2019-03-28 21:40:38 +0200

    added jdk and apt addon in travis yml

--- src/test/java/counter/DatabaseCounterTest.java
+++ src/test/java/counter/DatabaseCounterTest.java
@@ -1,8 +1,6 @@
 package counter;
-
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
