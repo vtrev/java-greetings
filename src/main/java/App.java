540a2a9bcb3112454ecb1f2d8b92723f86755868
vtrev <baloyi.trev@gmail.com>
2019-03-28 11:50:07 +0200

    lost the inputProccesor class and moved that logic into the app

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -9,25 +9,45 @@ import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) throws SQLException {
-
         Scanner scanner = new Scanner(System.in);
         Greeter greeter = new Greeter();
         MemoryCounter memoryCounter = new MemoryCounter();
         Connection dbConnection = DriverManager.getConnection("jdbc:h2:file:./database/greetings", "sa", "");
         DatabaseCounter databaseCounter = new DatabaseCounter(dbConnection);
-        CommandProcessor commandProcessor = new CommandProcessor();
-        InputProcessor cpu = new InputProcessor(greeter, databaseCounter, commandProcessor);
-
+        CommandProcessor cp = new CommandProcessor();
+        Action action = new Action(cp,databaseCounter,greeter);
         System.out.println("Hello World! Welcome to the greeting app! :)");
 
-        while (true) {
+        menu: while (true) {
             System.out.println("Enter a command : ");
-            boolean result = cpu.processInput(scanner.nextLine());
-            if (result == false) {
-                dbConnection.close();
-                break;
+            cp.processCommand(scanner.nextLine());
+            switch (cp.getCommand()) {
+                case ("greet"):
+                    System.out.println(action.greet());
+                    return;
+                case ("greeted"):
+                    System.out.println(action.count());
+                    break;
+                case ("clear"):
+                    if (action.clear()) {
+                        System.out.println("Clear successful");
+                        break;
+                    }
+                    System.out.println("Error while clearing");
+                    break;
+                case ("help"):
+                    System.out.println("Help");
+                    break;
+                case ("exit"):
+                    System.out.println(" Thanks for using the app.Goodbye!");
+                    dbConnection.close();
+                    break menu;
+                default:
+                    System.out.println("Error! Invalid command, please try again or type help for a valid  command list.");
             }
+
         }
 
     }
+
 }
