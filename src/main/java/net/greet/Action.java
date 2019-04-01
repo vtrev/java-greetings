8584b7ed022ff57cad8bb92b2d4ec4f73d38d702
vtrev <baloyi.trev@gmail.com>
2019-04-01 15:17:24 +0200

    moved print lines from main class

--- src/main/java/net/greet/Action.java
+++ src/main/java/net/greet/Action.java
@@ -1,7 +1,8 @@
 package net.greet;
 
 import net.greet.command.CommandProcessor;
-import net.greet.counter.*;
+import net.greet.counter.Counter;
+
 import java.sql.SQLException;
 
 public class Action {
@@ -16,24 +17,45 @@ public class Action {
         this.greeter = greeter;
     }
 
-    public String greet()throws SQLException{
+    public void greet()throws SQLException{
         if (commandProcessor.getName() == null) {
-            return "Please add your name after the greet net.greet.command.";
+            System.out.println("Please add your name after the greet command.");
+            return;
         }
-        return greeter.greet(commandProcessor.getName(),commandProcessor.getLangauge());
+        System.out.println(greeter.greet(commandProcessor.getName(),commandProcessor.getLangauge()));
     }
 
-    public String count() throws SQLException {
+    public void count() throws SQLException {
         if (commandProcessor.getName() != null) {
-            return "User " + commandProcessor.getName() + " has been greeted " + counter.userGreetCount(commandProcessor.getName()) + " times.";
+            System.out.println("User " + commandProcessor.getName() + " has been greeted " + counter.userGreetCount(commandProcessor.getName()) + " times.");
         }
-        return  "Total number of users greeted : " + counter.totalGreetCount();
+        System.out.println("Total number of users greeted : " + counter.totalGreetCount());
     }
 
-    public boolean clear() throws SQLException{
+    public void clear() throws SQLException{
         if (commandProcessor.getName() != null) {
-            return counter.clearUserCount(commandProcessor.getName());
+            if(counter.clearUserCount(commandProcessor.getName())){
+                System.out.println("User: "+ commandProcessor.getName()+" cleared successful");
+            }
+        }else if (counter.clearAllUserCounts()){
+                System.out.println("All users cleared successful");
+        }else {
+           System.out.println("Error while clearing");
         }
-        return  counter.clearAllUserCounts();
     }
+    public void getHelp(){
+        System.out.println("Greetings Console App Help Page \n General Commands : [greet,greeted,clear,help,exit]. \n Synopses \n" +
+                "1. greet [username] [option] - Returns a greeting on the screen. \n " +
+                "\t Username(required) : The name of user to greet. \n " +
+                "\t Option : A language in which the user will be greeted in .\n" +
+                "2. greeted [option] - Returns the number of times a user has been greeted. \n " +
+                "\t Option : The name of the user to get count for. \n"  +
+                "3. clear [option] - Removes the count from database. \n" +
+                " \t Option : The name of the user to clear,clears every user if no option is given .\n"  +
+                "4. help - Prints out current screen. \n" +
+                "5. exit - Exits the app. \n");
+    }
+//    public String displayResult(Function <f>){
+//        return
+//    }
 }
--- src/main/java/net/greet/Main.java
+++ src/main/java/net/greet/Main.java
@@ -22,33 +22,20 @@ public class Main {
 
         menu:
         while (true) {
-            System.out.println("Enter a net.greet.command : ");
+            System.out.println("Enter a command : ");
             cp.processCommand(scanner.nextLine());
             switch (cp.getCommand()) {
                 case ("greet"):
-                    System.out.println(action.greet());
+                    action.greet();
                     break;
                 case ("greeted"):
-                    System.out.println(action.count());
+                    action.count();
                     break;
                 case ("clear"):
-                    if (action.clear()) {
-                        System.out.println("Clear successful");
-                        break;
-                    }
-                    System.out.println("Error while clearing");
+                    action.clear();
                     break;
                 case ("help"):
-                    System.out.println("Greetings Console App Help Page \n General Commands : [greet,greeted,clear,help,exit]. \n Synopses \n" +
-                            "1. greet [username] [option] - Returns a greeting on the screen. \n " +
-                            "\t Username(required) : The name of user to greet. \n " +
-                            "\t Option : A language in which the user will be greeted in .\n" +
-                            "2. greeted [option] - Returns the number of times a user has been greeted. \n " +
-                            "\t Option : The name of the user to get count for. \n"  +
-                            "3. clear [option] - Removes the count from database. \n" +
-                            " \t Option : The name of the user to clear,clears every user if no option is given .\n"  +
-                            "4. help - Prints out current screen. \n" +
-                            "5. exit - Exits the app. \n");
+                    action.getHelp();
                     break;
                 case ("exit"):
                     System.out.println(" Thanks for using the app.Goodbye!");
