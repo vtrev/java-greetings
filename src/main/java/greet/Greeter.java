8b8508c98b6903bc74df8a5b907e27a79d26de32
Vusi Baloyi <baloyi.trev@gmail.com>
2021-12-31 17:18:53 +0200

    removed useless getLang method and unknown / empty cases from the switch

--- src/main/java/greet/Greeter.java
+++ src/main/java/greet/Greeter.java
@@ -5,32 +5,18 @@ import greet.command.Command;
 public class Greeter{
 
     public String greet(Command command){
-         Language language = command.getLanguage();
-         switch (language){
+         Languages languages = command.getLanguages();
+         switch (languages){
              case ENGLISH:
                  return "Hello "+command.getName()+"!";
              case TSONGA:
                  return "Avuxeni "+command.getName()+"!";
              case AFRIKAANS:
                  return "Hallo "+command.getName()+"!";
-             case UNKNOWN:
-                 return "Hi "+command.getName()+" we do not support the above language right now, please see help for available languages.";
-             case EMPTY:
-                     return "Hi " + command.getName() + " please enter a language you'd like to be greeted in after your name.";
              default:
                  return "Error!";
          }
     }
-    private Language getLang(String langString){
-        try {
-            return Language.valueOf(langString.toUpperCase());
-        }
-        catch (NullPointerException | IllegalArgumentException e){
-            //return default language
-            return Language.UNKNOWN;
-        }
-    }
-
 }
 
 
